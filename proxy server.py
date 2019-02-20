cdd8eedda46c759fcd7d35c371ae2592e16d3277
nikhil-polymath <nagasainikhil100@gmail.com>
2019-02-20 21:38:23 +0530

    Add files via upload

--- proxy server.py
+++ proxy server.py
@@ -1,9 +1,12 @@
+#upto now this program only works with GET requests , you can modify for browser requests
+
 import socket
 import sqlite3
 import time
 import re
 
 def pushtodb(data,ip,domain):
+    #add your database here
     connector=sqlite3.connect("logs.db")
     c=connector.cursor()
     timestamp=time.asctime(time.localtime())
@@ -22,8 +25,6 @@ client,address=client_to_proxy.accept()
 
 request=client.recv(4096).decode()
 
-#write your own function here
-
 
 #extracting url from GET request
 print(request)
@@ -37,11 +38,17 @@ for every in match:
     domain_name=every.group(0)
     print(domain_name)
 
+#write your own desired function
+
 pushtodb(request,client.getpeername()[0],domain_name)
+
+
+#stripping protocol from url as sockets gets error when we use protocol
 domain1=domain_name.replace('https://','')
 
 print(domain1)
 
+#creating socket which connects to server client wants to and sends response to client
 proxy_to_server=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 proxy_to_server.connect((domain1,80))
 
