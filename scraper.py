6eac777bb9319cc9ac904eab79857fbdb291196f
nikhil-polymath <nagasainikhil100@gmail.com>
2019-03-23 13:08:25 +0530

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -1,10 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
-redmi_page=requests.get('https://www.flipkart.com/search?q=redmi+note+7+pro&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_0_7&otracker1=AS_QueryStore_OrganicAutoSuggest_0_7&as-pos=0&as-type=RECENT&as-backfill=on')
+#redmi_page=requests.get('https://www.flipkart.com/search?q=redmi+note+7+pro&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_0_7&otracker1=AS_QueryStore_OrganicAutoSuggest_0_7&as-pos=0&as-type=RECENT&as-backfill=on')
+redmi_page=requests.get('https://www.flipkart.com/search?q=redmi+note+7+pro&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_0_5&otracker1=AS_QueryStore_OrganicAutoSuggest_0_5&as-pos=0&as-type=RECENT&as-backfill=on')
+
 #print(redmi_page.text)
 
-soup_page=BeautifulSoup(redmi_page.text,"lxml")
+soup_page=BeautifulSoup(redmi_page.text,"html.parser")
 #print(soup_page)
 
 #getting phones names
@@ -28,10 +31,11 @@ for every in range(0,len(comingsoon)):
 
 
 #monitoring items
-
-while True:
-    newpage=requests.get("https://www.flipkart.com/search?q=redmi+note+7+pro&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_0_7&otracker1=AS_QueryStore_OrganicAutoSuggest_0_7&as-pos=0&as-type=RECENT&as-backfill=on")
-    newsoup=BeautifulSoup(newpage.text)
+test=0
+while test!=1:
+    newpage=requests.get('https://www.flipkart.com/search?q=redmi+note+7+pro&sid=tyy%2C4io&as=on&as-show=on&otracker=AS_QueryStore_OrganicAutoSuggest_0_5&otracker1=AS_QueryStore_OrganicAutoSuggest_0_5&as-pos=0&as-type=RECENT&as-backfill=on')
+    
+    newsoup=BeautifulSoup(newpage.text,"html.parser")
     phone=newsoup.find_all(class_="_3wU53n")
     newphone=[]
     for i in range(0,len(phone)):
@@ -42,11 +46,22 @@ while True:
     elif len(redmi)>len(phone):
         print("some phones are deleted")
     else:
-        #monitoring prices
-        for every in range(0,len(redmi_prices)):
-            if redmi_prices[every]==prices[every].text:
-                pass
-            
+        #print(redmi_prices)
+        #print(prices)
+        for i in range(0,len(redmi_prices)):
+            print(redmi_prices[i].text,end="     ")
+            print(prices[i].text)
+        test=1
+            #print("product prices have changed")
+##        #monitoring prices
+##        for every in range(0,len(redmi_prices)):
+##            #print(redmi_prices[every])
+##            #print(prices[every].text)
+##            if redmi_prices[every].text==prices[every].text:
+##                pass
+##            else:
+##                print("product prices have changed")
+    #time.sleep(1)        
     
 
     
